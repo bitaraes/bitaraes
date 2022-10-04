7c6e349cc86f3bf966c1f74c66b4da00536c4c2a
Igor Bitarães <igor_oliveira02@hotmail.com>
2022-10-04 00:19:59 -0300

    [add] Adicionado Menu mobile

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-header">
     <div class="app-header__container">
-      <div class="name">Igor Bitarães</div>
+      <router-link to="/" class="app-header__name"> Igor Bitarães </router-link>
       <nav class="menu">
         <ul class="list">
           <li v-for="(item, index) in menu" :key="index" class="list-item">
@@ -11,11 +11,26 @@
       </nav>
     </div>
     <div class="m-menu">
-      <button class="open-menu">
+      <button class="open-menu" @click.stop="navOpen = !navOpen">
         <span class="top"></span>
         <span class="mid"></span>
         <span class="bottom"></span>
       </button>
+      <transition name="translateX">
+        <nav v-show="navOpen" @click.stop>
+          <div class="m-menu__wrapper">
+            <ul class="m-menu__list">
+              <li
+                v-for="(item, index) in menu"
+                :key="index"
+                class="m-menu__item"
+              >
+                <router-link :to="item.link">{{ item.name }}</router-link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </transition>
     </div>
   </div>
 </template>
@@ -24,12 +39,27 @@ export default {
   data() {
     return {
       menu: [
-        { name: "Sobre mim", link: "#" },
-        { name: "Skills", link: "#" },
-        { name: "Projetos", link: "#" },
-        { name: "Contato", link: "#" },
+        { name: "Sobre mim", link: "/sobre" },
+        { name: "Skills", link: "/skills" },
+        { name: "Projetos", link: "/projetos" },
+        { name: "Contato", link: "/contato" },
       ],
+      events: ["click", "scroll"],
+      navOpen: false,
     };
+  },
+  created() {
+    this.events.forEach((e) => window.addEventListener(e, this.toggle));
+  },
+  destroyed() {
+    this.events.forEach((e) => window.removeEventListener(e, this.toggle));
+  },
+  methods: {
+    toggle() {
+      if (this.navOpen) {
+        this.navOpen = false;
+      }
+    },
   },
 };
 </script>
@@ -39,6 +69,10 @@ export default {
   background: #0d0c22;
   width: 100%;
   height: 50px;
+}
+
+.app-header__name {
+  color: #fff;
 }
 
 .app-header__container {
@@ -87,11 +121,14 @@ export default {
   .app-header__container .menu {
     display: none;
   }
+  .app-header__name {
+    padding-inline: 15px;
+  }
   .open-menu {
     display: block;
     width: 50px;
     height: 50px;
-    background: grey;
+    background: #0d0c22;
     border: none;
     position: relative;
     z-index: 100;
@@ -117,6 +154,51 @@ export default {
   }
   .open-menu > span.bottom {
     transform: translateY(8px);
+  }
+  .m-menu nav {
+    width: 200px;
+    background: #0d0c22;
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 99;
+  }
+  .m-menu__wrapper {
+    padding-top: 50px;
+  }
+  .m-menu__list {
+    padding-top: 50px;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  .m-menu__item a {
+    text-decoration: none;
+    line-height: 1.6em;
+    font-size: 1.6em;
+    padding: 0.5em;
+    display: block;
+    color: white;
+    transition: 0.4s ease;
+  }
+  .m-menu__item a:hover {
+    background: lightgrey;
+    color: #0d0c22;
+  }
+  .translateX-enter {
+    transform: translateX(200px);
+    opacity: 0;
+  }
+
+  .translateX-enter-active,
+  .translateX-leave-active {
+    transform-origin: top right 0;
+    transition: 0.4s ease;
+  }
+
+  .translateX-leave-to {
+    transform: translateX(200px);
+    opacity: 0;
   }
 }
 </style>
